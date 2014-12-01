02aceaf54cdf64c91e7d3a67736490f57133edac
CarolinGuelpen <carolinguelpen@web.de>
2014-12-01 17:12:50 +0100

    restructured project
    -> separate files/packages for models and contentproviders

--- mapapps-basic-workflow/bundles/md2_app_ReferenceProject/module.js
+++ mapapps-basic-workflow/bundles/md2_app_ReferenceProject/module.js
@@ -1,10 +1,5 @@
 define([
     "ct/Stateful",
-    "./contentproviders/ComplaintProvider",
-    "./contentproviders/LocalComplaintProvider",
-    "./contentproviders/__returnStepStackProvider",
-    "./contentproviders/__processChainControllerStateProvider",
     "./Controller",
-    "./CustomActions",
-    "./Models"
+    "./CustomActions"
 ], {});
---
+++ mapapps-basic-workflow/bundles/md2_content_providers/module.js
@@ -0,0 +1,6 @@
+define([
+    "./contentproviders/ComplaintProvider",
+    "./contentproviders/LocalComplaintProvider",
+    "./contentproviders/__returnStepStackProvider",
+    "./contentproviders/__processChainControllerStateProvider"
+], {});
