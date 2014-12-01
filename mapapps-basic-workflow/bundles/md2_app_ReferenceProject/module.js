fdca7bcd1219f02a71f97484953ae48c0a90cacb
CarolinGuelpen <carolinguelpen@web.de>
2014-12-01 16:07:56 +0100

    generated App for Reference Implementation

---
+++ mapapps-basic-workflow/bundles/md2_app_ReferenceProject/module.js
@@ -0,0 +1,10 @@
+define([
+    "ct/Stateful",
+    "./contentproviders/ComplaintProvider",
+    "./contentproviders/LocalComplaintProvider",
+    "./contentproviders/__returnStepStackProvider",
+    "./contentproviders/__processChainControllerStateProvider",
+    "./Controller",
+    "./CustomActions",
+    "./Models"
+], {});
