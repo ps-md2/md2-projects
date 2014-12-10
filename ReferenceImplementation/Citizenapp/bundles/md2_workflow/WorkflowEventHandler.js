f95c4d89385e81deb45e8d664b78d8dbd768b4cb
CarolinGuelpen <carolinguelpen@web.de>
2014-12-10 21:30:02 +0100

    workflow event handler - second results
    no errors -- but event handler cannot collect controllers
    (aaaaaaaCitizenapp.controllers)

--- ReferenceImplementation/Citizenapp/bundles/md2_workflow/WorkflowEventHandler.js
+++ ReferenceImplementation/Citizenapp/bundles/md2_workflow/WorkflowEventHandler.js
@@ -1,11 +1,18 @@
 define([
-    "dojo/_base/declare"
+    "dojo/_base/declare",
+    "dojo/_base/array",
+    "ct/array", 
+    "ct/async"
 ],
-function(declare) {
+function(declare, d_array, ct_array, ct_async) {
     
     return declare([], {
         createInstance: function() {
             alert("hallo");
+            return {
+                handleEvent: this.handleEvent,
+                addController: this.addController
+            }
         },
         
         handleEvent: function(event, workflowelement) {
@@ -16,6 +23,23 @@ function(declare) {
            }
             
              
+        },
+        
+        addController: function (controller) {
+            var id = controller.get("id"),
+                    controllers = this.controllers;
+            //controllers.set(id, controller);
+            //if (this.activated && !this._initToolsetDefs) {
+            //    ct_array.arrayAdd(this._controllersToAdd || (this._controllersToAdd = []), controller);
+            //    var task = this._addControllerTask || (this._addControllerTask = ct_async.task(function () {
+            //        var controllersToAdd = this._controllersToAdd;
+            //        this._controllersToAdd = [];
+            //        d_array.forEach(controllersToAdd, function (controller) {
+            //            this._addControllerToControllersets(controller, id);
+            //        }, this);
+            //    }, this));
+            //    task.delay(100);
+            //}
         }
         
     });
