5a5521a543d0b83e7983f269c32238fce566e2d2
Jan <jan.dagefoerde@googlemail.com>
2014-12-10 14:37:57 +0100

    fixed wfe mediacapturing

--- ReferenceImplementation/Citizenapp/bundles/md2_wfe_Mediacapturing/CustomActions.js
+++ ReferenceImplementation/Citizenapp/bundles/md2_wfe_Mediacapturing/CustomActions.js
@@ -4,9 +4,6 @@ define([
     "./actions/__startupAction",
     "./actions/__processChainExecuteStepAction",
     "./actions/__processChainProcessingAction",
-    "./actions/__processChainActionEventTrigger_1f944c2608efb3bc6c96b04bd80b2b741c845906",
-    "./actions/__processChainActionEventTrigger_29aa495c4648acd7679bf7db841b2b794cc0f928",
-    "./actions/__processChainActionEventTrigger_fc57a13b5249bdb62057460aeb90add3169032bd",
     "./actions/__registerProcessChainActionEventTrigger",
     "./actions/__processChainSetProcessChainMyWorkflowAction"
 ],
@@ -16,9 +13,6 @@ function(
     __startupAction,
     __processChainExecuteStepAction,
     __processChainProcessingAction,
-    __processChainActionEventTrigger_1f944c2608efb3bc6c96b04bd80b2b741c845906,
-    __processChainActionEventTrigger_29aa495c4648acd7679bf7db841b2b794cc0f928,
-    __processChainActionEventTrigger_fc57a13b5249bdb62057460aeb90add3169032bd,
     __registerProcessChainActionEventTrigger,
     __processChainSetProcessChainMyWorkflowAction
 ) {
@@ -34,9 +28,6 @@ function(
                 new __startupAction(),
                 new __processChainExecuteStepAction(),
                 new __processChainProcessingAction(),
-                new __processChainActionEventTrigger_1f944c2608efb3bc6c96b04bd80b2b741c845906(),
-                new __processChainActionEventTrigger_29aa495c4648acd7679bf7db841b2b794cc0f928(),
-                new __processChainActionEventTrigger_fc57a13b5249bdb62057460aeb90add3169032bd(),
                 new __registerProcessChainActionEventTrigger(),
                 new __processChainSetProcessChainMyWorkflowAction()
             ];
