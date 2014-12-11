23b45cbb7f7738895e7658a0b4cda71b516d5488
CarolinGuelpen <carolinguelpen@web.de>
2014-12-11 12:25:46 +0100

    WorkflowEventHandler works now :-)
    TODO: arm access to private attribute "_isFirstExecution" -> should be
    solved differently in the future

--- ReferenceImplementation/Citizenapp/bundles/md2_workflow/WorkflowEventHandler.js
+++ ReferenceImplementation/Citizenapp/bundles/md2_workflow/WorkflowEventHandler.js
@@ -8,45 +8,33 @@ function(declare, Hash) {
             this.controllers = new Hash();
         },
         createInstance: function() {
-            alert("hallo");
             return {
                 handleEvent: this.handleEvent,
                 addController: this.addController,
-                removeController: this.removeController
+                removeController: this.removeController,
+                instance: this
             };
         },
         
         handleEvent: function(event, workflowelement) {
            if (event === "LocationEvent" && workflowelement === "Locationdetection")
            {
-               // TODO get correct controller from list this.controllers
-              var wfe = this._mediacapturingController;
-              wfe.activate();
-           }
-            
+              // TODO: Accessing private attribute _isFirstExecution should be solved somehow differently in the future! 
              
+              this.instance.controllers.get("md2.wfe.Locationdetection.Controller").closeWindow();
+              this.instance.controllers.get("md2.wfe.Locationdetection.Controller")._isFirstExecution = true;
+              this.instance.controllers.get("md2.wfe.MediaCapturing.Controller").openWindow();
+              
+           }
+   
         },
         
         addController: function (controller, properties) {
-            console.log("controller", controller);
-            console.log("properties", properties);
-            var id = controller.get("id"),
-                    controllers = this.controllers;
-            
-            // TODO put controller into list this.controllers
-            
-            //controllers.set(id, controller);
-            //if (this.activated && !this._initToolsetDefs) {
-            //    ct_array.arrayAdd(this._controllersToAdd || (this._controllersToAdd = []), controller);
-            //    var task = this._addControllerTask || (this._addControllerTask = ct_async.task(function () {
-            //        var controllersToAdd = this._controllersToAdd;
-            //        this._controllersToAdd = [];
-            //        d_array.forEach(controllersToAdd, function (controller) {
-            //            this._addControllerToControllersets(controller, id);
-            //        }, this);
-            //    }, this));
-            //    task.delay(100);
-            //}
+           // console.log("controller", controller);
+           // console.log("properties", properties);
+           // console.log("properties", properties.objectClass[0]);
+            this.controllers.set(properties.objectClass[0],controller);
+ 
         },
         
         removeController: function (controller, properties) {
