d79956a5a51709cfe37f967a8099bcbd2d8f153f
Jan <jan.dagefoerde@googlemail.com>
2015-01-26 13:40:13 +0100

    add workflow element succession information to Config bean

--- ReferenceImplementation/ReferenceProject.backend/src/ReferenceProject/backend/Config.java
+++ ReferenceImplementation/ReferenceProject.backend/src/ReferenceProject/backend/Config.java
@@ -21,18 +21,51 @@ public class Config {
 	 * to allow backward compatibility.
 	 */
 	public final static List<String> SUPPORTED_MODEL_VERSIONS = Lists.newArrayList("2.0");
-	
-	public final static HashMap<String, String[]> apps = setApps();
+
+	public final static HashMap<String, String[]> APP_WORKFLOWELEMENT_RELATIONSHIP = setAppWorkflowElementRelationship();
+
+	public final static HashMap<String, HashMap<String, String>> WORKFLOWELEMENT_EVENT_SUCCESSION = setAppWorkflowElementSuccession();
 	
 	/**
 	 * provides a hashmap for filtering workflowelements by apps
+	 * setAppWorkflowElementRelationship : (App) --> (Wfe)*
 	 * @return
 	 */
-	private static HashMap<String,String[]> setApps(){
+	private static HashMap<String,String[]> setAppWorkflowElementRelationship(){
 		
 		HashMap<String, String[]> map = new HashMap<String, String[]>();
 		map.put("Citizenapp", new String[]{"LocationDetection", "Mediacapturing", "SubmitComplaint"});
 		map.put("Administrationapp", new String[]{"ProcessComplaint"});
+		return map;
+	}
+
+	
+	/**
+	 * Given an event-throwing app and a thrown event, this map knows what workflow element has to follow
+	 * setAppWorkflowElementSuccession : (Wfe x Event) --> Wfe
+	 * @return
+	 */
+	private static HashMap<String, HashMap<String, String>> setAppWorkflowElementSuccession() {
+		HashMap<String, HashMap<String, String>> map = new HashMap<String, HashMap<String, String>>();
+		HashMap<String, String> innerMap;
+
+		// Coming from LocationDetection
+		innerMap = new HashMap<String, String>();
+		innerMap.put("LocationEvent", "Mediacapturing");
+		map.put("LocationDetection", innerMap);
+		
+
+		// Coming from Mediacapturing
+		innerMap = new HashMap<String, String>();
+		innerMap.put("MediacapturedEvent", "SubmitComplaint");
+		map.put("Mediacapturing", innerMap);
+		
+		// Coming from SubmitComplaint
+		innerMap = new HashMap<String, String>();
+		innerMap.put("SubmitEvent", "ProcessComplaint");
+		map.put("SubmitComplaint", innerMap);		
+			
+
 		return map;
 	}
 }
