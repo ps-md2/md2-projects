2b53dc0af6c9b275b4dc4d9124088d43dfbd2072
CarolinGuelpen <carolinguelpen@web.de>
2015-02-24 15:32:34 +0100

    External Dummy WS works now

--- ReferenceImplementation/CurrentStateProject.backend/src/CurrentStateProject/backend/ws/ExternalDummyService.java
+++ ReferenceImplementation/CurrentStateProject.backend/src/CurrentStateProject/backend/ws/ExternalDummyService.java
@@ -5,10 +5,12 @@
 package CurrentStateProject.backend.ws;
 
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -16,7 +18,7 @@ import CurrentStateProject.backend.Config;
 
 
 
-@Path("/externalWS")
+@Path("/externalDummyWS")
 @Stateless
 public class ExternalDummyService {
 
@@ -24,21 +26,18 @@ public class ExternalDummyService {
 	@GET
 	@Path("/sum")
 	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
-	public Response get(@FormParam("float1") float float1, @FormParam("float2") float float2,
-			@FormParam("float3") float float3) {
+	public Response get(@QueryParam("float1") float float1, @QueryParam("float2") float float2,
+			@QueryParam("float3") float float3) {
 	
 		float sum = float1 + float2 + float3;
-	
+		System.out.println("!!!!!!!!!!!!!!!!!!!" + sum);
 		
 		return Response
 				.ok()
-				.entity(sum)
 				.header("MD2-Model-Version", Config.MODEL_VERSION)
 				.build();	
 		
 	}
 
-	
-	
 	
 }
