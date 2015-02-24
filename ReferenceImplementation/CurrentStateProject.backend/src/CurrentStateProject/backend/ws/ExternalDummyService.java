2423dd2774940d4b75e13aa190ff455216b834dd
Malte Möser <mail@maltemoeser.de>
2015-02-24 16:22:11 +0100

    Add dummy POST webservice

--- ReferenceImplementation/CurrentStateProject.backend/src/CurrentStateProject/backend/ws/ExternalDummyService.java
+++ ReferenceImplementation/CurrentStateProject.backend/src/CurrentStateProject/backend/ws/ExternalDummyService.java
@@ -5,9 +5,8 @@
 package CurrentStateProject.backend.ws;
 
 import javax.ejb.Stateless;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -15,7 +14,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import CurrentStateProject.backend.Config;
-
 
 
 @Path("/externalDummyWS")
@@ -28,16 +26,29 @@ public class ExternalDummyService {
 	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
 	public Response get(@QueryParam("float1") float float1, @QueryParam("float2") float float2,
 			@QueryParam("float3") float float3) {
-	
+		
 		float sum = float1 + float2 + float3;
-		System.out.println("!!!!!!!!!!!!!!!!!!!" + sum);
 		
 		return Response
 				.ok()
+				.entity(sum)
 				.header("MD2-Model-Version", Config.MODEL_VERSION)
 				.build();	
-		
 	}
-
 	
+	
+	@POST
+	@Path("/sum")
+	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+	public Response post(@QueryParam("float1") float float1, @QueryParam("float2") float float2,
+			@QueryParam("float3") float float3) {
+		
+		float sum = float1 + float2 + float3;
+		
+		return Response
+				.ok()
+				.entity(sum)
+				.header("MD2-Model-Version", Config.MODEL_VERSION)
+				.build();	
+	}
 }
