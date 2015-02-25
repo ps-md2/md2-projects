ee6eac532b216dfdeb360b6391759efc35d0d017
Malte Möser <mail@maltemoeser.de>
2015-02-25 13:18:32 +0100

    Refactor and reformat

--- ReferenceImplementation/CurrentStateProject.backend/src/CurrentStateProject/backend/ws/CallExternalWebServiceWS.java
+++ ReferenceImplementation/CurrentStateProject.backend/src/CurrentStateProject/backend/ws/CallExternalWebServiceWS.java
@@ -12,30 +12,29 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
 import CurrentStateProject.backend.Config;
-import CurrentStateProject.backend.beans.ComplaintBean;
 import CurrentStateProject.backend.entities.RequestDTO;
-
 import CurrentStateProject.backend.entities.RequestDTO.RequestType;
 
 @Path("/externalWS")
 @Stateless
 public class CallExternalWebServiceWS {
-
-
+	
+	
+	/**
+	 * Receives a json-encoded object containing a url, a REST method type and a set of parameters.
+	 * Based on this data, a new request is created.
+	 * @param dto contains the request data.
+	 * @return A response signaling success or failure of the request.
+	 */
 	@POST
 	@Path("/callExternalWS")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -44,97 +43,87 @@ public class CallExternalWebServiceWS {
 		Boolean responseOk = true;
 		int code = 0;
 		try {
-		  		URL url;
-		  		RequestType type = dto.getRequestType();
+			URL url;
+			RequestType type = dto.getRequestType();
 
-		  		// build URL
-		  		switch(type) {
-		  		case GET:
-			  		String urlParams = "?";
-	
-					for(Entry<String, Object> entry: dto.getParams().entrySet()) {
-						urlParams += entry.getKey() + "=" + entry.getValue() + "&";		
-					}
-					// remove trailing &
-					urlParams = urlParams.substring(0, urlParams.length()-1);
-				  	url = new URL(dto.getUrl() + urlParams); 
-		  		
-				  	break;
-		  		case POST:
-		  			url = new URL(dto.getUrl());  	
-		  			break;
-		  		default:
-		  			url = new URL(dto.getUrl());
-				}
+			// build URL
+			switch (type) {
+			case GET:
+				String urlParams = buildUrlParameters(dto.getParams());
+				url = new URL(dto.getUrl() + urlParams);
+				break;
+			case POST:
+				url = new URL(dto.getUrl());
+				break;
+			default:
+				url = new URL(dto.getUrl());
+			}
 
-	  			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-				conn.setRequestMethod(type.toString());
-				conn.setRequestProperty("Content-Type", "application/json");
-	  							
-		  		switch(dto.getRequestType()) {
-		  		case GET:
-		  			conn.setDoOutput(false);
-		  			break;
-		  		case POST: 
-					conn.setDoOutput(true);
-
-					String postParams = JSONValue.toJSONString(dto.getParams());
-					System.out.println(postParams);
-					
-					OutputStream os = conn.getOutputStream();
-					os.write(postParams.getBytes());
-					os.flush();
-					break;
-		  		case PUT:  	
-		  			break;
-		  		case DELETE:
-		  			break;
-		  		default: 
-		  			break;
-				}
-		  		
-				if(conn.getResponseCode() != 200){
-					responseOk = false;
-				}
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod(type.toString());
+			conn.setRequestProperty("Content-Type", "application/json");
+			
+			// Add method-specific options and content
+			switch (type) {
+			case GET:
+				conn.setDoOutput(false);
+				break;
+			case POST:
+				conn.setDoOutput(true);
 				
-				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-		 
-				String output;
-				System.out.println("Output from Server .... \n");
-				while ((output = br.readLine()) != null) {
-					System.out.println(output);
-				}
-				code = conn.getResponseCode();
-				conn.disconnect();
-		  		
-			  } catch (MalformedURLException e) {
-					 
-				e.printStackTrace();
-		 
-			  } catch (IOException e) {
-		 
-				e.printStackTrace();
-		 
-			 }				
-		
-		  
-			if(responseOk){
-				return Response
-					.ok()
-					.header("MD2-Model-Version", Config.MODEL_VERSION)
-					.build();	
+				// JSON-encode parameter HashMap
+				String postParams = JSONValue.toJSONString(dto.getParams());
+				
+				OutputStream os = conn.getOutputStream();
+				os.write(postParams.getBytes());
+				os.flush();
+				break;
+			case PUT:
+				break;
+			case DELETE:
+				break;
+			default:
+				break;
 			}
-			else{
-				return Response
-					.status(code) // TODO: Change status here
-					.header("MD2-Model-Version", Config.MODEL_VERSION)
-					.build();	
+
+			if (conn.getResponseCode() != 200) {
+				responseOk = false;
 			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+			code = conn.getResponseCode();
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if (responseOk) {
+			return Response.ok()
+					.header("MD2-Model-Version", Config.MODEL_VERSION).build();
+		} else {
+			return Response.status(code) // TODO: Change status here
+					.header("MD2-Model-Version", Config.MODEL_VERSION).build();
+		}
 	}
 	
-}
-	
-	
-	
-	
+	private String buildUrlParameters(HashMap<String, Object> params){
+		String urlParams = "?";
 
+		for (Entry<String, Object> entry : params.entrySet()) {
+			urlParams += entry.getKey() + "=" + entry.getValue() + "&";
+		}
+		// remove trailing "&"
+		urlParams = urlParams.substring(0, urlParams.length() - 1);
+		return urlParams;
+	}
+}
