44665f40e49a156783ac73a72216e2d21f8c6e1b
Jan <jan.dagefoerde@googlemail.com>
2015-02-25 18:21:05 +0100

    mappings to output

--- ReferenceImplementation/CurrentStateProject.mapapps/md2_app_CurrentStateApp/bundles/md2_wfe_Mediacapturing/actions/Init.js
+++ ReferenceImplementation/CurrentStateProject.mapapps/md2_app_CurrentStateApp/bundles/md2_wfe_Mediacapturing/actions/Init.js
@@ -13,6 +13,10 @@ function(declare, _Action) {
             var contentProvider0aw = this.$.contentProviderRegistry.getContentProvider("complaintProvider");
             var widget0ax = this.$.widgetRegistry.getWidget("fileupload");
             this.$.dataMapper.map(widget0ax, contentProvider0aw, "picture");
+            
+            var contentProvider0aw = this.$.contentProviderRegistry.getContentProvider("complaintProvider");
+            var widget0ax = this.$.widgetRegistry.getWidget("imageoutput");
+            this.$.dataMapper.map(widget0ax, contentProvider0aw, "picture");
         }
         
     });
