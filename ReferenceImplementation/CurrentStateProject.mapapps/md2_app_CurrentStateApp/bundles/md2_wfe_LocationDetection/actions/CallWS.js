eaaa322b9bc894efa7ef5b1ace69c62b7f204973
Malte Möser <mail@maltemoeser.de>
2015-02-26 17:46:38 +0100

    Use whenFunction to retrieve content at a later point in time

--- ReferenceImplementation/CurrentStateProject.mapapps/md2_app_CurrentStateApp/bundles/md2_wfe_LocationDetection/actions/CallWS.js
+++ ReferenceImplementation/CurrentStateProject.mapapps/md2_app_CurrentStateApp/bundles/md2_wfe_LocationDetection/actions/CallWS.js
@@ -10,16 +10,29 @@ function(declare, _Action) {
         
         execute: function() {
             
-            var widget1337 = this.$.widgetRegistry.getWidget("DetectLocationBtn");
-            var action1337 = this.$.actionFactory.getWebServiceCallAction(
-                    "http://localhost:8080/CurrentStateProject.backend/service/externalDummyWS/sum",
-                    "POST",
-                    null,
-                    {"float1": 3.4,
-                    "float2": 22.4,
-                    "float3": 1.4}
-            ); 
+            var widget1337 = this.$.widgetRegistry.getWidget("Next2");
+            var that = this;
+            var whenFunction = function(){
+                return that.$.contentProviderRegistry.getContentProvider("addressProvider").getValue("myLatitude");
+            };
+            
+            var action1337 = this.$.actionFactory.getWebServiceCallAction("http://localhost:8080/CurrentStateProject.backend/service/externalDummyWS/sum", "POST", {
+            }, {
+                "float1": 3.4, 
+                "float2": 22.4, 
+                "float3": whenFunction
+            });
             this.$.eventRegistry.get("widget/onClick").registerAction(widget1337, action1337);
+            
+            var widget1338 = this.$.widgetRegistry.getWidget("Next2");
+            
+            var action1338 = this.$.actionFactory.getWebServiceCallAction("http://psmd2.uni-muenster.de:8080/SendMail/api/mail/send/", "GET", {
+                "to": "andreas.fuchs@uni-muenster.de", 
+                "subject": "Hallo Andreas", 
+                "body": "Diese E-Mail stammt aus map.apps!" 
+            }, {
+            });
+            this.$.eventRegistry.get("widget/onClick").registerAction(widget1338, action1338);
             
         }
         
